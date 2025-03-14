852b0061a241a02a90d03b27806c84ca83436d6a
adilraees786 <adilraees911@gmail.com>
2025-03-14 16:21:23 +0500

    add image

--- src/compoments/UserCard.jsx
+++ src/compoments/UserCard.jsx
@@ -5,10 +5,11 @@ const UserCard = (props) => {
     
   return (
     <div>
-    
+    <img src={props.data.picture.large} alt="" />
         <h3>{props.data.name.first}</h3>
         <h6>{props.data.gender}</h6>
         <p>{props.data.phone}</p>
+        <p>{props.data.email}</p>
    <p>{props.data.location.city}, {props.data.location.state}</p>
 
 
